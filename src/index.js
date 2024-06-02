bd9da7129ad287ed987da5864c54c46fa9b72b10
dduutt <dote27@163.com>
2024-06-02 19:56:38 +0800

    引入kv配置项

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/store";
+import dotenv from "dotenv";
 
 import App from "./App";
+
+dotenv.config()
 
 ReactDOM.render(
   <Provider store={configureStore}>
